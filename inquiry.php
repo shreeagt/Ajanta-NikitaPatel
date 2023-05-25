da88a56db79fa46497e1f967c83cda729f45bfb0
RahulRawatAgt <rahulrawat.agt@gmail.com>
2023-05-25 17:33:01 +0530

    removed the link of the disease and treatment

--- inquiry.php
+++ inquiry.php
@@ -182,7 +182,7 @@ background-color: #476BA1;
 
                                                </li>
                                                <li class="dropdown">
-                                                   <a href="patient.html">Treatment/Facilities</a>
+                                                   <a href="#">Treatment/Facilities</a>
                                                       <ul>
                                                          <li><a href="cataract.html">Cataract</a></li>
                                                          <li><a href="lasik.html">Lasik</a></li>
@@ -190,7 +190,7 @@ background-color: #476BA1;
                                                    <span class="righticon"><i class="fa fa-angle-down"></i></span>
                                               </li>
                                               <li class="dropdown">
-                                               <a href="patient-services.html">Disease</a>
+                                               <a href="#">Disease</a>
                                                    <ul>
                                                          <li><a href="cataract.html">Cataract</a></li>
                                                          <li><a href="lasik.html">Lasik</a></li>
