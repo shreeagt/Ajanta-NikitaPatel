42d27f10a43b22d3e744f318c1d060764f39feb5
RahulRawatAgt <rahulrawat.agt@gmail.com>
2024-01-05 13:49:34 +0530

    contact js  added

--- js/contact.js
+++ js/contact.js
@@ -6,8 +6,9 @@ $(document).ready(function () {
       var name = $.trim($('input[name="name"]').val());
       var email = $.trim($('input[name="email"]').val());
       var phone = $.trim($('input[name="phone"]').val());
-      var field1 = $.trim($('input[name="field1"]').val());
-      var message = document.getElementById('message').value;
+      var subject = "contact added by user";
+      var message = "contact added by user";
+      var age = $.trim($('input[name="age"]').val());
 
       // Validate the email using a regular expression
       var doctorToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZTY3OWIzNGFjOGIwNDQyMDkyZGJkYTc5NmMyODE4ZTkwYjJkNDA1OTA2ZDBjMzVhYzM1ZTljODFhYzQ4YjdkZTEzYmE4ZDIxYTM3NDc3MDIiLCJpYXQiOjE3MDI5OTgzODYuOTk4MjM2LCJuYmYiOjE3MDI5OTgzODYuOTk4MjM4LCJleHAiOjE3MzQ2MjA3ODYuOTgxODI5LCJzdWIiOiI1Iiwic2NvcGVzIjpbXX0.oQK58sMfJA09f4W8KWucHLIrfMYSc_o_myNOm8XQq5disF-LCDt8uIqgx7dyZ77FgUVEf56_HMlvdfwTuQQZxYDL3qB25BMHobVal4_yYn92nL81Jgi_JqnRjK1imI-GWRqmI8g_S4Fd7zbCftx2layhQpGzPRrh7rwchMPVPw3lHdJa1FdjH28HkxY_tKZt6XTseJeQuLQhsNWj5QroaDqxMgaPutreUqvF_MJzynI6RdrCiJGr5FEaEmhUaDmcMRno8WDPNAJCDk_-hUgG5yhfmXQ9WAVgF1K83_jYtsQcoFy4qmEhUUyNznareWmFo0kbvk3bZubu0mfPiulPnHZtsBnxdZo_9UIwNpAooOKGL9hiYOsxDe7rfD0BWwccpZwnYdqBcxQAYwF0b9cI0wie7epkOIrv4Um_g5RTEeLOYY0Bnti06TM-cL_lvt5YqFBMJ81eXVs8AQP8vxi3CJaxJQCmGu5W5BA1TGTGd-Htg4uK-0pzbnUWTztD_y5sI04N6TVs6yhnC_8EVYDuqA4-7yeE5WNV9JiIoQC_m7FTBlOjIbopYD1IDUokrL8TASZddD0D8xuMYlBwRaEnIGROgtf1ki9pk0p5M2kSaHgboJ6qutaFK9uCZug6uutv-3hyMKl75eycOkLlL8DrsgROLc9sI6F0p6AwDz4ku5E';
@@ -18,8 +19,9 @@ $(document).ready(function () {
           name: name,
           email: email,
           phone: phone,
-          field1: field1,
+          subject:subject,
           message: message,
+          field1: age,
       }
       $.ajax({
           // url: '/new-user',
